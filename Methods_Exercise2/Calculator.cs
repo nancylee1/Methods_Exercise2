301b15f0b0a13bba32fb1d9166e5bdefa921f3d7
Nancy Lee <107009879+nancylee1@users.noreply.github.com>
2022-07-05 11:54:11 -0400

    initial commit

---
+++ Methods_Exercise2/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_Exercise2
+{
+    internal class Calculator
+    {
+        public static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Sum(int x, int y, int z)
+        { 
+            return x + y + z; 
+        }
+
+        public static int Sum(int a, int b, int c, int d, int e)
+            { return a + b + c + d + e; }
+
+        public static int Multiply(int x, int y)
+
+        { return x * y; }
+
+        public static int Divide(int x, int y)
+            { return x / y; }
+    }
+}
---
+++ Methods_Exercise2/Program.cs
@@ -0,0 +1,7 @@
+﻿using Methods_Exercise2;
+
+Console.WriteLine(Calculator.Sum(2, 4));
+Console.WriteLine(Calculator.Multiply(10, 2));
+Console.WriteLine(Calculator.Divide(10, 2));
+Console.WriteLine(Calculator.Sum(2, 4, 6));
+Console.WriteLine(Calculator.Sum(1,1,1,1,1));
